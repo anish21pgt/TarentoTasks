5454cc69038381f04b093f268190cb2a1488740d
unknown <anish.pgt@gmail.com>
2023-05-25 18:14:49 +0530

    committing FirstPgm.java

---
+++ FirstPgm.java
@@ -0,0 +1,10 @@
+public class FirstPgm
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World");
+		System.out.println("This is Anish");
+		System.out.println("Modifying my program to see changes");
+		System.out.println("Trying to modify again");
+	}
+}
